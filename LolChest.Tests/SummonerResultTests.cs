97bd288158f7e95a8cf5a9dead633295e081997b
Marcel Neumann <marcel.neumann@draeger.com>
2021-10-23 09:58:57 +0200

    Add unit tests for penalty and ToString overload

--- LolChest.Tests/SummonerResultTests.cs
+++ LolChest.Tests/SummonerResultTests.cs
@@ -42,5 +42,61 @@ namespace LolChest.Tests
 
             Assert.AreEqual("DFF Quackerjack", sut.SummonerName);
         }
+
+        [TestMethod]
+        public void Won_CorrectPenalty()
+        {
+            var kda = new Kda(4, 1, 12);
+            var duration = TimeSpan.FromMinutes(21);
+            var gameCreation = new DateTime(2019, 5, 13);
+
+            var sut = new SummonerResult("Whizzmaster",
+                                         61,
+                                         kda,
+                                         duration,
+                                         true,
+                                         "123456",
+                                         gameCreation);
+
+            decimal expectedResult = Math.Round((decimal)(1.0 / (4 + 12 + 1.0) * 21.0 * 0.05), 2);
+            Assert.AreEqual(expectedResult, sut.GetPenalty());
+        }
+
+        [TestMethod]
+        public void Lost_CorrectPenalty()
+        {
+            var kda = new Kda(4, 1, 12);
+            var duration = TimeSpan.FromMinutes(21);
+            var gameCreation = new DateTime(2019, 5, 13);
+
+            var sut = new SummonerResult("Whizzmaster",
+                                         61,
+                                         kda,
+                                         duration,
+                                         false,
+                                         "123456",
+                                         gameCreation);
+
+            decimal expectedResult = Math.Round((decimal)(1.0 / (4 + 12 + 1.0) * 21.0 * 0.05 * 1.5), 2);
+            Assert.AreEqual(expectedResult, sut.GetPenalty());
+        }
+
+        [TestMethod]
+        public void CorrectToString()
+        {
+            var kda = new Kda(4, 1, 12);
+            var duration = TimeSpan.FromMinutes(21);
+            var gameCreation = new DateTime(2019, 5, 13);
+
+            var sut = new SummonerResult("Homer Simpson",
+                                         61,
+                                         kda,
+                                         duration,
+                                         true,
+                                         "123456",
+                                         gameCreation);
+
+            Assert.AreEqual("Homer Simpson (Orianna): 4/1/12 ==> 0,06€", sut.ToString());
+        }
     }
 }
