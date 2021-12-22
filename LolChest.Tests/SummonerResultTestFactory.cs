fb759647b7597d482a011a47249338ce5f2b10cf
Marcel Neumann <marcel.neumann@draeger.com>
2021-12-22 13:20:56 +0100

    Extracted creation of test summoner results to a factory class.

---
+++ LolChest.Tests/SummonerResultTestFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using LolChest.Core;
+
+namespace LolChest.Tests
+{
+    internal static class SummonerResultTestFactory
+    {
+        internal static SummonerResult CreateSummonerResult(string name = "Homer Simpson",
+                                                            int championId = 61,
+                                                            bool won = true,
+                                                            string gameId = "123456",
+                                                            int kills = 4,
+                                                            int deaths = 1,
+                                                            int assists = 12,
+                                                            int durationMinutes = 21,
+                                                            int creationYear = 2019,
+                                                            int creationMonth = 5,
+                                                            int creationDay = 13,
+                                                            EFirstBloodParticipation firstBloodParticipation = EFirstBloodParticipation.NoParticipation)
+        {
+            return new SummonerResult(name,
+                                      championId,
+                                      new Kda(kills, deaths, assists),
+                                      TimeSpan.FromMinutes(durationMinutes),
+                                      won,
+                                      gameId,
+                                      new DateTime(creationYear, creationMonth, creationDay),
+                                      firstBloodParticipation);
+        }
+    }
+}
--- LolChest.Tests/SummonerResultTests.cs
+++ LolChest.Tests/SummonerResultTests.cs
@@ -12,21 +12,21 @@ namespace LolChest.Tests
         [TestMethod]
         public void IfNameIsHolgerHodensack_ReplaceWithDFFFiesoduck()
         {
-            SummonerResult sut = CreateSummonerResult("Holger Hodensack");
+            SummonerResult sut = SummonerResultTestFactory.CreateSummonerResult("Holger Hodensack");
             Assert.AreEqual("DFF Fiesoduck", sut.SummonerName);
         }
 
         [TestMethod]
         public void IfNameIsWhizzmaster_ReplaceWithDFFQuackerjack()
         {
-            SummonerResult sut = CreateSummonerResult("Whizzmaster");
+            SummonerResult sut = SummonerResultTestFactory.CreateSummonerResult("Whizzmaster");
             Assert.AreEqual("DFF Quackerjack", sut.SummonerName);
         }
 
         [TestMethod]
         public void Won_CorrectPenalty()
         {
-            SummonerResult sut = CreateSummonerResult("Whizzmaster");
+            SummonerResult sut = SummonerResultTestFactory.CreateSummonerResult("Whizzmaster");
 
             decimal expectedResult = Math.Round((decimal)(1.0 / (4 + 12 + 1.0) * 21.0 * 0.05), 2);
             Assert.AreEqual(expectedResult, sut.GetPenalty());
@@ -35,7 +35,7 @@ namespace LolChest.Tests
         [TestMethod]
         public void Lost_CorrectPenalty()
         {
-            SummonerResult sut = CreateSummonerResult("Whizzmaster", won: false);
+            SummonerResult sut = SummonerResultTestFactory.CreateSummonerResult("Whizzmaster", won: false);
 
             decimal expectedResult = Math.Round((decimal)(1.0 / (4 + 12 + 1.0) * 21.0 * 0.05 * 1.5), 2);
             Assert.AreEqual(expectedResult, sut.GetPenalty());
@@ -44,7 +44,7 @@ namespace LolChest.Tests
         [TestMethod]
         public void FirstBloodWon_CorrectPenalty()
         {
-            SummonerResult sut = CreateSummonerResult("Whizzmaster", firstBloodParticipation: EFirstBloodParticipation.WonFirstBlood);
+            SummonerResult sut = SummonerResultTestFactory.CreateSummonerResult("Whizzmaster", firstBloodParticipation: EFirstBloodParticipation.WonFirstBlood);
 
             decimal expectedResult = Math.Round((decimal)(1.0 / (4 + 12 + 1.0) * 21.0 * 0.05 - 0.05), 2);
             Assert.AreEqual(expectedResult, sut.GetPenalty());
@@ -53,7 +53,7 @@ namespace LolChest.Tests
         [TestMethod]
         public void CorrectToString()
         {
-            SummonerResult sut = CreateSummonerResult();
+            SummonerResult sut = SummonerResultTestFactory.CreateSummonerResult();
 
             Assert.AreEqual("Homer Simpson (Orianna): 4/1/12 ==> 0,06€", sut.ToString());
         }
@@ -61,10 +61,10 @@ namespace LolChest.Tests
         [TestMethod]
         public void TestGetSummonersExtension()
         {
-            SummonerResult sr1 = CreateSummonerResult("Homer");
-            SummonerResult sr2 = CreateSummonerResult("Marge");
-            SummonerResult sr3 = CreateSummonerResult("Homer");
-            SummonerResult sr4 = CreateSummonerResult("Marge");
+            SummonerResult sr1 = SummonerResultTestFactory.CreateSummonerResult("Homer");
+            SummonerResult sr2 = SummonerResultTestFactory.CreateSummonerResult("Marge");
+            SummonerResult sr3 = SummonerResultTestFactory.CreateSummonerResult("Homer");
+            SummonerResult sr4 = SummonerResultTestFactory.CreateSummonerResult("Marge");
 
             var summonerResults = new List<SummonerResult>
             {
@@ -83,10 +83,10 @@ namespace LolChest.Tests
         [TestMethod]
         public void TestOfExtension()
         {
-            SummonerResult sr1 = CreateSummonerResult("Homer");
-            SummonerResult sr2 = CreateSummonerResult("Marge");
-            SummonerResult sr3 = CreateSummonerResult("Homer");
-            SummonerResult sr4 = CreateSummonerResult("Marge");
+            SummonerResult sr1 = SummonerResultTestFactory.CreateSummonerResult("Homer");
+            SummonerResult sr2 = SummonerResultTestFactory.CreateSummonerResult("Marge");
+            SummonerResult sr3 = SummonerResultTestFactory.CreateSummonerResult("Homer");
+            SummonerResult sr4 = SummonerResultTestFactory.CreateSummonerResult("Marge");
 
             var summonerResults = new List<SummonerResult>
             {
@@ -104,16 +104,16 @@ namespace LolChest.Tests
         [TestMethod]
         public void TestGetLolChestSummonerResults()
         {
-            SummonerResult sr1 = CreateSummonerResult("Homer", won: true, gameId: "1");
-            SummonerResult sr2 = CreateSummonerResult("Homer", won: true, gameId: "2");
-            SummonerResult sr3 = CreateSummonerResult("Homer", won: true, gameId: "3");
+            SummonerResult sr1 = SummonerResultTestFactory.CreateSummonerResult("Homer", won: true, gameId: "1");
+            SummonerResult sr2 = SummonerResultTestFactory.CreateSummonerResult("Homer", won: true, gameId: "2");
+            SummonerResult sr3 = SummonerResultTestFactory.CreateSummonerResult("Homer", won: true, gameId: "3");
 
-            SummonerResult sr4 = CreateSummonerResult("Marge", won: true, gameId: "1");
+            SummonerResult sr4 = SummonerResultTestFactory.CreateSummonerResult("Marge", won: true, gameId: "1");
 
-            SummonerResult sr5 = CreateSummonerResult("Marge", won: true, gameId: "2");
+            SummonerResult sr5 = SummonerResultTestFactory.CreateSummonerResult("Marge", won: true, gameId: "2");
 
-            SummonerResult sr6 = CreateSummonerResult("Bart", won: true, gameId: "3");
-            SummonerResult sr7 = CreateSummonerResult("Bart", won: true, gameId: "4");
+            SummonerResult sr6 = SummonerResultTestFactory.CreateSummonerResult("Bart", won: true, gameId: "3");
+            SummonerResult sr7 = SummonerResultTestFactory.CreateSummonerResult("Bart", won: true, gameId: "4");
 
             var summonerResults = new List<SummonerResult>
             {
@@ -137,13 +137,39 @@ namespace LolChest.Tests
         }
 
         [TestMethod]
+        public void TestCountGames()
+        {
+            SummonerResult sr1 = SummonerResultTestFactory.CreateSummonerResult("Homer", won: true, gameId: "1");
+            SummonerResult sr2 = SummonerResultTestFactory.CreateSummonerResult("Homer", won: true, gameId: "2");
+            SummonerResult sr3 = SummonerResultTestFactory.CreateSummonerResult("Homer", won: true, gameId: "3");
+            SummonerResult sr4 = SummonerResultTestFactory.CreateSummonerResult("Marge", won: true, gameId: "1");
+            SummonerResult sr5 = SummonerResultTestFactory.CreateSummonerResult("Marge", won: true, gameId: "2");
+            SummonerResult sr6 = SummonerResultTestFactory.CreateSummonerResult("Bart", won: true, gameId: "3");
+            SummonerResult sr7 = SummonerResultTestFactory.CreateSummonerResult("Bart", won: true, gameId: "4");
+
+            var summonerResults = new List<SummonerResult>
+            {
+                sr1,
+                sr2,
+                sr3,
+                sr4,
+                sr5,
+                sr6,
+                sr7
+            };
+
+            int games = summonerResults.CountGames();
+            Assert.AreEqual(4, games);
+        }
+
+        [TestMethod]
         public void TestGetLolChestSummonerResults_Duplicate()
         {
-            SummonerResult sr1 = CreateSummonerResult("Homer", won: true, gameId: "1");
-            SummonerResult sr2 = CreateSummonerResult("Homer", won: true, gameId: "1");
+            SummonerResult sr1 = SummonerResultTestFactory.CreateSummonerResult("Homer", won: true, gameId: "1");
+            SummonerResult sr2 = SummonerResultTestFactory.CreateSummonerResult("Homer", won: true, gameId: "1");
 
-            SummonerResult sr3 = CreateSummonerResult("Marge", won: true, gameId: "1");
-            SummonerResult sr4 = CreateSummonerResult("Marge", won: true, gameId: "1");
+            SummonerResult sr3 = SummonerResultTestFactory.CreateSummonerResult("Marge", won: true, gameId: "1");
+            SummonerResult sr4 = SummonerResultTestFactory.CreateSummonerResult("Marge", won: true, gameId: "1");
 
             var summonerResults = new List<SummonerResult>
             {
@@ -162,54 +188,54 @@ namespace LolChest.Tests
         [TestMethod]
         public void TestPLotExtension()
         {
-            SummonerResult sr1 = CreateSummonerResult(name: "Homer",
-                                                      championId: 61,
-                                                      won: false,
-                                                      gameId: "1",
-                                                      kills: 5,
-                                                      deaths: 3,
-                                                      assists: 12,
-                                                      durationMinutes: 78,
-                                                      creationYear: 2021,
-                                                      creationMonth: 3,
-                                                      creationDay: 2);
+            SummonerResult sr1 = SummonerResultTestFactory.CreateSummonerResult(name: "Homer",
+                                                                                championId: 61,
+                                                                                won: false,
+                                                                                gameId: "1",
+                                                                                kills: 5,
+                                                                                deaths: 3,
+                                                                                assists: 12,
+                                                                                durationMinutes: 78,
+                                                                                creationYear: 2021,
+                                                                                creationMonth: 3,
+                                                                                creationDay: 2);
 
-            SummonerResult sr2 = CreateSummonerResult(name: "Homer",
-                                                      championId: 62,
-                                                      won: true,
-                                                      gameId: "2",
-                                                      kills: 12,
-                                                      deaths: 4,
-                                                      assists: 21,
-                                                      durationMinutes: 18,
-                                                      creationYear: 2020,
-                                                      creationMonth: 7,
-                                                      creationDay: 13,
-                                                      firstBloodParticipation: EFirstBloodParticipation.WonFirstBlood);
+            SummonerResult sr2 = SummonerResultTestFactory.CreateSummonerResult(name: "Homer",
+                                                                                championId: 62,
+                                                                                won: true,
+                                                                                gameId: "2",
+                                                                                kills: 12,
+                                                                                deaths: 4,
+                                                                                assists: 21,
+                                                                                durationMinutes: 18,
+                                                                                creationYear: 2020,
+                                                                                creationMonth: 7,
+                                                                                creationDay: 13,
+                                                                                firstBloodParticipation: EFirstBloodParticipation.WonFirstBlood);
 
-            SummonerResult sr3 = CreateSummonerResult(name: "Marge",
-                                                      championId: 12,
-                                                      won: false,
-                                                      gameId: "1",
-                                                      kills: 1,
-                                                      deaths: 0,
-                                                      assists: 6,
-                                                      durationMinutes: 78,
-                                                      creationYear: 2021,
-                                                      creationMonth: 3,
-                                                      creationDay: 2);
+            SummonerResult sr3 = SummonerResultTestFactory.CreateSummonerResult(name: "Marge",
+                                                                                championId: 12,
+                                                                                won: false,
+                                                                                gameId: "1",
+                                                                                kills: 1,
+                                                                                deaths: 0,
+                                                                                assists: 6,
+                                                                                durationMinutes: 78,
+                                                                                creationYear: 2021,
+                                                                                creationMonth: 3,
+                                                                                creationDay: 2);
 
-            SummonerResult sr4 = CreateSummonerResult(name: "Bart",
-                                                      championId: 67,
-                                                      won: true,
-                                                      gameId: "2",
-                                                      kills: 0,
-                                                      deaths: 0,
-                                                      assists: 0,
-                                                      durationMinutes: 18,
-                                                      creationYear: 2020,
-                                                      creationMonth: 7,
-                                                      creationDay: 13);
+            SummonerResult sr4 = SummonerResultTestFactory.CreateSummonerResult(name: "Bart",
+                                                                                championId: 67,
+                                                                                won: true,
+                                                                                gameId: "2",
+                                                                                kills: 0,
+                                                                                deaths: 0,
+                                                                                assists: 0,
+                                                                                durationMinutes: 18,
+                                                                                creationYear: 2020,
+                                                                                creationMonth: 7,
+                                                                                creationDay: 13);
 
             var summonerResults = new List<SummonerResult>
             {
@@ -221,29 +247,6 @@ namespace LolChest.Tests
 
             string plot = summonerResults.Plot();
             Assert.AreEqual(Resources.ExpectedPlot, plot);
-        }
-
-        private SummonerResult CreateSummonerResult(string name = "Homer Simpson",
-                                                    int championId = 61,
-                                                    bool won = true,
-                                                    string gameId = "123456",
-                                                    int kills = 4,
-                                                    int deaths = 1,
-                                                    int assists = 12,
-                                                    int durationMinutes = 21,
-                                                    int creationYear = 2019,
-                                                    int creationMonth = 5,
-                                                    int creationDay = 13,
-                                                    EFirstBloodParticipation firstBloodParticipation = EFirstBloodParticipation.NoParticipation)
-        {
-            return new SummonerResult(name,
-                                      championId,
-                                      new Kda(kills, deaths, assists),
-                                      TimeSpan.FromMinutes(durationMinutes),
-                                      won,
-                                      gameId,
-                                      new DateTime(creationYear, creationMonth, creationDay),
-                                      firstBloodParticipation);
         }
     }
 }
