ed4124db8bca887728e492c6c0123706c2f822a8
Marcel Neumann <marcel.neumann@draeger.com>
2021-12-23 09:31:19 +0100

    Add missing unit test file for Kda class. Finalize unit test for showing the yearly report.

---
+++ LolChest.Tests/KdaTests.cs
@@ -0,0 +1,36 @@
+﻿using LolChest.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LolChest.Tests
+{
+    [TestClass]
+    public class KdaTests
+    {
+        [TestMethod]
+        public void TestToString()
+        {
+            var sut = new Kda(42, 21, 99);
+            string result = sut.ToString();
+
+            Assert.AreEqual("42/21/99", result);
+        }
+
+        [TestMethod]
+        public void TestSingleDigitKda()
+        {
+            var sut = new Kda(42, 21, 99);
+            double result = sut.GetSingleDigitKda();
+
+            Assert.AreEqual((42.0 + 99.0) / 21.0, result);
+        }
+
+        [TestMethod]
+        public void TestSingleDigitKda_NoDeaths()
+        {
+            var sut = new Kda(42, 0, 99);
+            double result = sut.GetSingleDigitKda();
+
+            Assert.AreEqual(42.0 + 99.0, result);
+        }
+    }
+}
--- LolChest.Tests/YearlyReportTests.cs
+++ LolChest.Tests/YearlyReportTests.cs
@@ -12,16 +12,135 @@ namespace LolChest.Tests
         [TestMethod]
         public async Task TestYearlyReport()
         {
-            SummonerResult summonerResult1 = SummonerResultTestFactory.CreateSummonerResult("Cello", 61, true, "1", 4,2,12, 39, 2021, 12, 24, EFirstBloodParticipation.NoParticipation);
-            SummonerResult summonerResult2 = SummonerResultTestFactory.CreateSummonerResult("Cello", 62, false, "2", 2,1,24, 42, 2021, 9, 12, EFirstBloodParticipation.WonFirstBlood);
-            SummonerResult summonerResult3 = SummonerResultTestFactory.CreateSummonerResult("Cello", 62, true, "3", 13,15,8, 15, 2021, 7, 6, EFirstBloodParticipation.NotYetTracked);
-            SummonerResult summonerResult4 = SummonerResultTestFactory.CreateSummonerResult("Whizzy", 12, true, "1", 13,13,34, 39, 2021, 12, 24, EFirstBloodParticipation.WonFirstBlood);
-            SummonerResult summonerResult5 = SummonerResultTestFactory.CreateSummonerResult("Whizzy", 11, false, "2", 4,4,10, 42, 2021, 9, 12, EFirstBloodParticipation.WonFirstBlood);
-            SummonerResult summonerResult6 = SummonerResultTestFactory.CreateSummonerResult("Whizzy", 10, true, "3", 4,0,15, 15, 2021, 7, 6, EFirstBloodParticipation.NoParticipation);
-            SummonerResult summonerResult7 = SummonerResultTestFactory.CreateSummonerResult("Whizzy", 62, true, "4", 4,0,15, 15, 2021, 7, 6, EFirstBloodParticipation.NoParticipation);
-            SummonerResult summonerResult8 = SummonerResultTestFactory.CreateSummonerResult("Benni", 9, true, "1", 5,5,41, 39, 2021, 9, 24, EFirstBloodParticipation.NoParticipation);
-            SummonerResult summonerResult9 = SummonerResultTestFactory.CreateSummonerResult("Benni", 8, false, "2", 0,0,11, 42, 2021, 7, 12, EFirstBloodParticipation.NoParticipation);
-            SummonerResult summonerResult10 = SummonerResultTestFactory.CreateSummonerResult("Benni", 23, true, "4", 4,0,15, 15, 2021, 7, 6, EFirstBloodParticipation.NoParticipation);
+            SummonerResult summonerResult1 = SummonerResultTestFactory.CreateSummonerResult("Cello",
+                                                                                            61,
+                                                                                            true,
+                                                                                            "1",
+                                                                                            4,
+                                                                                            2,
+                                                                                            12,
+                                                                                            39,
+                                                                                            2021,
+                                                                                            12,
+                                                                                            24,
+                                                                                            EFirstBloodParticipation.NoParticipation);
+
+            SummonerResult summonerResult2 = SummonerResultTestFactory.CreateSummonerResult("Cello",
+                                                                                            62,
+                                                                                            false,
+                                                                                            "2",
+                                                                                            2,
+                                                                                            1,
+                                                                                            24,
+                                                                                            42,
+                                                                                            2021,
+                                                                                            9,
+                                                                                            12,
+                                                                                            EFirstBloodParticipation.WonFirstBlood);
+
+            SummonerResult summonerResult3 = SummonerResultTestFactory.CreateSummonerResult("Cello",
+                                                                                            62,
+                                                                                            true,
+                                                                                            "3",
+                                                                                            13,
+                                                                                            15,
+                                                                                            8,
+                                                                                            15,
+                                                                                            2021,
+                                                                                            7,
+                                                                                            6,
+                                                                                            EFirstBloodParticipation.NotYetTracked);
+
+            SummonerResult summonerResult4 = SummonerResultTestFactory.CreateSummonerResult("Whizzy",
+                                                                                            12,
+                                                                                            true,
+                                                                                            "1",
+                                                                                            13,
+                                                                                            13,
+                                                                                            34,
+                                                                                            39,
+                                                                                            2021,
+                                                                                            12,
+                                                                                            24,
+                                                                                            EFirstBloodParticipation.WonFirstBlood);
+
+            SummonerResult summonerResult5 = SummonerResultTestFactory.CreateSummonerResult("Whizzy",
+                                                                                            11,
+                                                                                            false,
+                                                                                            "2",
+                                                                                            4,
+                                                                                            4,
+                                                                                            10,
+                                                                                            42,
+                                                                                            2021,
+                                                                                            9,
+                                                                                            12,
+                                                                                            EFirstBloodParticipation.WonFirstBlood);
+
+            SummonerResult summonerResult6 = SummonerResultTestFactory.CreateSummonerResult("Whizzy",
+                                                                                            10,
+                                                                                            true,
+                                                                                            "3",
+                                                                                            4,
+                                                                                            0,
+                                                                                            15,
+                                                                                            15,
+                                                                                            2021,
+                                                                                            7,
+                                                                                            6,
+                                                                                            EFirstBloodParticipation.NoParticipation);
+
+            SummonerResult summonerResult7 = SummonerResultTestFactory.CreateSummonerResult("Whizzy",
+                                                                                            62,
+                                                                                            true,
+                                                                                            "4",
+                                                                                            4,
+                                                                                            0,
+                                                                                            15,
+                                                                                            15,
+                                                                                            2021,
+                                                                                            7,
+                                                                                            6,
+                                                                                            EFirstBloodParticipation.NoParticipation);
+
+            SummonerResult summonerResult8 = SummonerResultTestFactory.CreateSummonerResult("Benni",
+                                                                                            9,
+                                                                                            true,
+                                                                                            "1",
+                                                                                            5,
+                                                                                            5,
+                                                                                            41,
+                                                                                            39,
+                                                                                            2021,
+                                                                                            9,
+                                                                                            24,
+                                                                                            EFirstBloodParticipation.NoParticipation);
+
+            SummonerResult summonerResult9 = SummonerResultTestFactory.CreateSummonerResult("Benni",
+                                                                                            8,
+                                                                                            false,
+                                                                                            "2",
+                                                                                            0,
+                                                                                            0,
+                                                                                            11,
+                                                                                            42,
+                                                                                            2021,
+                                                                                            7,
+                                                                                            12,
+                                                                                            EFirstBloodParticipation.NoParticipation);
+
+            SummonerResult summonerResult10 = SummonerResultTestFactory.CreateSummonerResult("Benni",
+                                                                                             23,
+                                                                                             true,
+                                                                                             "4",
+                                                                                             4,
+                                                                                             0,
+                                                                                             15,
+                                                                                             15,
+                                                                                             2021,
+                                                                                             7,
+                                                                                             6,
+                                                                                             EFirstBloodParticipation.NoParticipation);
 
             var summonerResults = new List<SummonerResult>
             {
@@ -44,8 +163,6 @@ namespace LolChest.Tests
             var sut = new YearlyReport(bucket.Object);
 
             string report = await sut.Create("2021");
-
-            Assert.Inconclusive();
         }
     }
 }
